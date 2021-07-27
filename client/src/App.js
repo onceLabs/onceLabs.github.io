723d48bd2d0f498cde684f4c802f239518d10173
Jekko Syquia <jekkogray22@gwu.edu>
2021-07-27 12:49:40 -0400

    Initialized client and server

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flower: {}
+    }
+    this.getFlower();
+  }
+  getFlower() {
+    fetch('/flower')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          flower: data
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.flower.name}</h1>
+        <p>{this.state.flower.colour}</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express()
-const port = 8080;
+
+const port = process.env.HTTP_PORT || 8080; // default port
 
 app.get('/', (req, res) => res.send('Hello World.'));
+app.get('/fetchMe', (req, res) => res.send('You fetched me'));
+app.get('flower', (req, res) => res.json({ name: 'Dandelion', color: 'Blue' }))
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
