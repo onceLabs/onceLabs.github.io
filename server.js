a9f7fd7a5c1c9aaaa32e2b077417185f0ae3b939
Seth Polyniak <emailspolyniak@gmail.com>
2021-07-27 13:22:09 -0400

    Added build script to server side

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
-const port = 8080;
+import path from 'path';
+const port = process.env.HTTP_PORT || 4001;
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('/', (req, res) => res.send('Hello World.'));
 
